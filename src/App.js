603db1dd024005ed097430131788509c70b426e7
HaroldZS <101837004+HaroldZS@users.noreply.github.com>
2022-03-29 19:55:37 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navigation from './Components/Navigation';
+import LoginForm from './Components/LoginForm';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
